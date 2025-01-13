96b2fbbbf4ea04baedad3633c58ac087cc2cff64
Nirmal <pkdnirmal@gmail.com>
2025-01-13 12:51:32 -0600

    Added a new java file

---
+++ BS2D.java
@@ -0,0 +1,33 @@
+
+package bs2d;
+
+public class BS2D {
+
+
+    public static void main(String[] args) {
+     int [][] a = {
+         {10,20,30,40},
+         {11,25,35,45},
+         {28,29,37,49}, 
+         {33,34,38,50}
+     };
+     int target = 36;
+     System.out.println(Search(a,target));
+    }
+   static int Search(int[][] a,int target){
+       int row=0;int column=a[0].length-1;
+       for(int i=0;i<a.length;i++){
+           for(int j=column;j>=0;j--){
+               if(a[i][j] == target){
+                   return a[i][j];
+               }
+               if(a[i][j] > target){
+                   column = column -1;
+               }else{
+                   break;
+               }
+           }
+       }
+       return -1;
+   }
+}
